64e562fd436b0614a95ef5e8469445d525e766c6
mitsukawa <w074ff@yamaguchi-u.ac.jp>
2021-09-17 23:52:51 +0900

    fix: 複数の端末でアクセスした場合IMAGEが上書きされて正しく動作しないため

--- app.py
+++ app.py
@@ -2,13 +2,10 @@ from flask import Flask, request, render_template, redirect
 import base64
 from PIL import Image
 from io import BytesIO
-from flask.helpers import url_for
 import numpy as np
 
 # Flaskの設定
 app = Flask(__name__, )
-
-IMAGE = None  # 撮影した画像を格納する変数
 
 
 # トップページ
@@ -18,30 +15,32 @@ def index():
 
 
 # 撮影した画像を変数に格納してプレビュー画面を表示
-@app.route("/previewImage", methods=["GET","POST"])
+@app.route("/previewImage", methods=["POST"])
 def image_preview():
-    if request.method == 'POST':
+
+    if request.method == "POST":
         # 撮影した画像をデコード
-        global IMAGE
-        enc_img = request.form["image"]
-        IMAGE = base64.b64decode(enc_img.split(",")[1])
-
-        return render_template("preview.html", shotImage=enc_img)
-
+        img_base64 = request.form["image"]
+        return render_template("preview.html", shotImage=img_base64)
     else:
-        return redirect(url_for("/"))
+        return redirect("/")
 
 
 # 撮影した画像を基に画像処理を行って結果画面を表示
-@app.route("/result")
+@app.route("/result", methods=["POST"])
 def result():
-    # デコードした画像を読み込んでnumpyに変換
-    img_pil = Image.open(BytesIO(IMAGE))
-    image_np = np.array(img_pil)
-    print(image_np)
-    # これ以降に画像の処理を書いていく...
-
-    return "result"
+    if request.method == "POST":
+        # デコードした画像を読み込んでnumpyに変換
+        # print(request.form["image"])
+        img_base64 = request.form["image"]
+        image_binary = base64.b64decode(img_base64.split(",")[1])
+        img_pil = Image.open(BytesIO(image_binary))
+        image_np = np.array(img_pil)
+        print(image_np)
+        # これ以降に画像の処理を書いていく...
+        return '<image src="{}" width="{}"/>'.format(img_base64, "100%")
+    else:
+        return redirect("/")
 
 
 if __name__ == "__main__":
