d901ca1d991668bb02d65518a48b1fa65d84aa18
mitsukawa <w074ff@yamaguchi-u.ac.jp>
2021-09-17 17:14:05 +0900

    fix: デプロイの確認のため

--- app.py
+++ app.py
@@ -16,28 +16,28 @@ def index():
     return render_template("index.html")
 
 
-# # 撮影した画像を変数に格納してプレビュー画面を表示
-# @app.route("/previewImage", methods=["POST"])
-# def image_preview():
+# 撮影した画像を変数に格納してプレビュー画面を表示
+@app.route("/previewImage", methods=["POST"])
+def image_preview():
 
-#     # 撮影した画像をデコード
-#     global IMAGE
-#     enc_img = request.form["image"]
-#     IMAGE = base64.b64decode(enc_img.split(",")[1])
+    # 撮影した画像をデコード
+    global IMAGE
+    enc_img = request.form["image"]
+    IMAGE = base64.b64decode(enc_img.split(",")[1])
 
-#     return render_template("preview.html", shotImage=enc_img)
+    return render_template("preview.html", shotImage=enc_img)
 
 
-# # 撮影した画像を基に画像処理を行って結果画面を表示
-# @app.route("/result")
-# def result():
-#     # デコードした画像を読み込んでnumpyに変換
-#     img_pil = Image.open(BytesIO(IMAGE))
-#     image_np = np.array(img_pil)
-#     print(image_np)
-#     # これ以降に画像の処理を書いていく...
+# 撮影した画像を基に画像処理を行って結果画面を表示
+@app.route("/result")
+def result():
+    # デコードした画像を読み込んでnumpyに変換
+    img_pil = Image.open(BytesIO(IMAGE))
+    image_np = np.array(img_pil)
+    print(image_np)
+    # これ以降に画像の処理を書いていく...
 
-#     return "result"
+    return "result"
 
 
 if __name__ == "__main__":
