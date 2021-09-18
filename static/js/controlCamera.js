2c630b3b4bad9a547187b4fcb0d83eb45f15e85e
mitsukawa <w074ff@yamaguchi-u.ac.jp>
2021-09-18 12:16:38 +0900

    fix: 解像度の修正

--- static/js/controlCamera.js
+++ static/js/controlCamera.js
@@ -41,12 +41,12 @@ function controlCamera(videoId, canvasId, shutterButtonId, formImageId) {
     // 撮影ボタンが押されたときのイベント処理（<video>の1フレームを<canvas>に表示）
     document.getElementById(shutterButtonId).addEventListener("click", () => {
       const canvasContext = canvas.getContext("2d");
-      const rate = 1
-      const w = video.offsetWidth*rate;
-      const h = video.offsetHeight*rate;
+      const rate = 2
+      const w = video.offsetWidth/rate;
+      const h = video.offsetHeight/rate;
       canvas.setAttribute("width", w.toString());
       canvas.setAttribute("height", h.toString());
-      canvasContext.drawImage(video, 0, 0, w, h);
+      canvasContext.drawImage(video, 0, 0, video.offsetWidth, video.offsetHeight);
       formImage.value = canvas.toDataURL("image/png");
     });
   };
